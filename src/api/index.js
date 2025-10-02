c383aff76ca1986f252f3b42bdf31ff2ab5c212d
SACHIN KOLI <132149727+Sachinkoligit@users.noreply.github.com>
2025-10-02 17:18:58 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: "event-frontend-orcin.vercel.app",
     credentials: true,
   })
 );
