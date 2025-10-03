d72e4d028f80c9325d5616db2588f00f54647b52
Sachin <sachinkoli8477@gmail.com>
2025-10-03 12:37:21 +0530

    updated index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import authRoutes from "./src/routes/auth.route.js";
 import eventRoutes from "./src/routes/event.route.js";
 import adminRoutes from "./src/routes/admin.route.js";
+import bookingRoutes from "./src/routes/booking.route.js";
 import { configDotenv } from "dotenv";
 import { connect } from "./src/lib/db.js";
 import cookieParser from "cookie-parser";
@@ -15,18 +16,19 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://event-frontend-orcin.vercel.app",
     credentials: true,
   })
 );
 
 app.get("/", (req, res) => {
-  return res.send("Backend is live 🚀");
+  return res.send("Backend is live ");
 });
 
 app.use("/api/auth", authRoutes);
 app.use("/api/event", eventRoutes);
 app.use("/api/admin", adminRoutes);
+app.use("/api/booking",bookingRoutes)
 
 app.listen(5001, () => {
   console.log("Server is Running on:" + PORT);
